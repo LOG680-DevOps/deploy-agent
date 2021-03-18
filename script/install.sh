11c1aa14975c0be8efc9a3b52a587d10b7b24ec8
Maxime Thomassin <maxthomassin@hotmail.com>
2021-03-18 13:58:57 -0400

    fu

--- script/install.sh
+++ script/install.sh
@@ -7,3 +7,4 @@ sudo chmod +x /lib/systemd/system/deployagent.service
 sudo systemctl daemon-reload
 sudo systemctl enable deployagent.service
 sudo systemctl status deployagent.service
+systemd-analyze verify /usr/lib/systemd/system/deployagent.service
--- script/launch.sh
+++ script/launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 git pull
+ls
 go run src/main.go -password TwoPizzaRule
