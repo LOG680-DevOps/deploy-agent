2fae51d91e739aada0bfd7fd1b69732a43f111e0
Maxime Thomassin <maxthomassin@hotmail.com>
2021-03-18 13:31:37 -0400

    added more script

---
+++ script/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo yum install golang -y
+
+sudo cp deployagent.service /lib/systemd/system/
+sudo chmod +x /lib/systemd/system/deployagent.service
+sudo systemctl daemon-reload
+sudo systemctl enable deployagent.service
+sudo systemctl status deployagent.service
---
+++ script/launch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git pull
+go run src/main.go -password TwoPizzaRule
